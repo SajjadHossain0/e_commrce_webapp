9b2510ae786e11e1fef965f931bedd3e01f6ef40
mdsaj <mdsajjadhossain614@gmail.com>
2024-09-22 13:29:00 +0600

    new commit

---
+++ src/main/java/com/ecommrce/e_commrce_webapp/ECommrceWebappApplication.java
@@ -0,0 +1,13 @@
+package com.ecommrce.e_commrce_webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommrceWebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommrceWebappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ecommrce/e_commrce_webapp/ECommrceWebappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecommrce.e_commrce_webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommrceWebappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
