d42abb3a07eb809afb29d8f5b7efe565cfa3004b
mdsaj <mdsajjadhossain614@gmail.com>
2024-09-26 20:27:34 +0600

    new commit

--- src/main/java/com/ecommrce/e_commrce_webapp/Controllers/HomeController.java
+++ src/main/java/com/ecommrce/e_commrce_webapp/Controllers/HomeController.java
@@ -23,4 +23,10 @@ public class HomeController {
 
         return "view/view_product";
     }
+
+    @GetMapping("/seller")
+    public String seller(){
+
+        return "seller/seller_dashboard";
+    }
 }
