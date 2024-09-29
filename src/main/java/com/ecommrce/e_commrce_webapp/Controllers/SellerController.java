ff870547dc35f770e3dcb540a90d04f74b88c2c6
mdsaj <mdsajjadhossain614@gmail.com>
2024-09-30 01:20:48 +0600

    new commit

--- src/main/java/com/ecommrce/e_commrce_webapp/Controllers/SellerController.java
+++ src/main/java/com/ecommrce/e_commrce_webapp/Controllers/SellerController.java
@@ -2,16 +2,44 @@ package com.ecommrce.e_commrce_webapp.Controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/seller")
 public class SellerController {
 
 
-    @GetMapping("/seller")
+    @GetMapping("/seller_dashboard")
     public String seller(){
 
         return "seller/seller_dashboard";
     }
+
+    @GetMapping("/add_product")
+    public String add_product(){
+
+        return "seller/add_product";
+    }
+
+    @GetMapping("/manage_categories")
+    public String manage_categories(){
+
+        return "seller/manage_categories";
+    }
+
+    @GetMapping("/view_orders")
+    public String view_orders(){
+
+        return "seller/view_orders";
+    }
+
+    @GetMapping("/view_products")
+    public String view_products(){
+
+        return "seller/view_products";
+    }
+
+
 }
 
 /*
