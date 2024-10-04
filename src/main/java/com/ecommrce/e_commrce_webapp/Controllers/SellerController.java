093373477c49cbc9dd6003013e22f87500a28c6d
mdsaj <mdsajjadhossain614@gmail.com>
2024-10-04 21:16:14 +0600

    new commit

--- src/main/java/com/ecommrce/e_commrce_webapp/Controllers/SellerController.java
+++ src/main/java/com/ecommrce/e_commrce_webapp/Controllers/SellerController.java
@@ -21,6 +21,12 @@ import java.util.List;
 @Controller
 @RequestMapping("/seller")
 public class SellerController {
+    @Autowired
+    private CategoryService categoryService;
+    @Autowired
+    private SubCategoryService subCategoryService;
+    @Autowired
+    private ProductService productService;
 
 
     @GetMapping("/seller_dashboard")
@@ -28,13 +34,6 @@ public class SellerController {
 
         return "seller/seller_dashboard";
     }
-
-    @Autowired
-    private CategoryService categoryService;
-    @Autowired
-    private SubCategoryService subCategoryService;
-    @Autowired
-    private ProductService productService;
 
     @GetMapping("/add_product")
     public String add_product(Model model){
@@ -92,17 +91,68 @@ public class SellerController {
         return "redirect:/seller/seller_dashboard";
     }
 
+    @PostMapping("/edit-product")
+    public String edit_product(@RequestParam("id") Long id,@RequestParam("product_image") MultipartFile product_image,
+                              @RequestParam("product_image1") MultipartFile product_image1, @RequestParam("product_image2") MultipartFile product_image2,
+                              @RequestParam("product_image3") MultipartFile product_image3, @RequestParam("title") String title,
+                              @RequestParam("price") Double price, @RequestParam("category") String category,
+                              @RequestParam("sub_category") String sub_category, @RequestParam("stock") int stock,
+                              @RequestParam("short_description") String short_description, @RequestParam("detailed_description") String detailed_description,
+                              @RequestParam("product_details") String product_details){
+
+        Product product = productService.getProductById(id);
+        // Convert the cover photo to a Base64 string
+        String encodedImage = null;
+        String encodedImage1 = null;
+        String encodedImage2 = null;
+        String encodedImage3 = null;
+        try {
+            encodedImage = Base64.getEncoder().encodeToString(product_image.getBytes());
+            encodedImage1 = Base64.getEncoder().encodeToString(product_image1.getBytes());
+            encodedImage2 = Base64.getEncoder().encodeToString(product_image2.getBytes());
+            encodedImage3 = Base64.getEncoder().encodeToString(product_image3.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        product.setProduct_image(encodedImage);
+        product.setProduct_image1(encodedImage1);
+        product.setProduct_image2(encodedImage2);
+        product.setProduct_image3(encodedImage3);
+        product.setTitle(title);
+        product.setPrice(price);
+        product.setCategory(category);
+        product.setSub_category(sub_category);
+        product.setStock(stock);
+        product.setShort_description(short_description);
+        product.setDetailed_description(detailed_description);
+        product.setProduct_details(product_details);
+
+        productService.saveProduct(product);
+
+        return "redirect:/seller/seller_dashboard";
+    }
+
+    @GetMapping("/deleteProduct/{id}")
+    public String deleteProduct(@PathVariable("id") Long id) {
+        productService.deleteProduct(id);
+        return "redirect:/seller/view_products";
+    }
+
     @GetMapping("/view_orders")
     public String view_orders(){
 
         return "seller/view_orders";
     }
 
-
     @GetMapping("/view_products")
     public String view_products(@AuthenticationPrincipal User user, Model model){
         List<Product> products = productService.getProductsByUser(user);
         model.addAttribute("products", products);
+
+        List<Category> categories = categoryService.getAllCategories();
+        List<SubCategory> subCategories = subCategoryService.getAllSubCategories();
+        model.addAttribute("categories", categories);
+        model.addAttribute("subCategories", subCategories);
 
         return "seller/view_products";
     }
